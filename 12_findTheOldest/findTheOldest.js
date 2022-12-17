e7f0a17cc463bd9b7aca72a361349dd4e0f57978
Tanguy <tanguy.freycon@gmail.com>
2022-12-17 18:10:05 +0100

    Completes 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((obj, item) => {
+        if (!('yearOfDeath' in obj)) {
+            obj.yearOfDeath = (new Date).getFullYear();
+        }
+        if (!('yearOfDeath' in item)) {
+            item.yearOfDeath = (new Date).getFullYear();
+        }
+        if (item.yearOfDeath - item.yearOfBirth > obj.yearOfDeath - obj.yearOfBirth) obj = item;
+        return obj;
+    });
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 2018,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+
+console.log(!('yearOfDeath' in people[0]));
+console.log(findTheOldest(people));
+
