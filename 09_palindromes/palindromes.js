3b1d3ed9b0c3ff4bb0d46447f528bbbabf322910
Tanguy <tanguy.freycon@gmail.com>
2022-12-17 15:22:20 +0100

    Completes 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regEx = /^[0-9a-zA-Z]+$/;
+    string = string.split('');
+    string = string.filter(char => char.match(regEx));
+    string = string.map(char => char.toLowerCase());
+    return string.every((element, index) => {
+        return string[string.length - index-1] === element;
+    });
 };
 
 // Do not edit below this line
