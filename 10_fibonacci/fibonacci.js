2a9208ea2dbdfb116492bc40d58470d8baf2a0fd
Tanguy <tanguy.freycon@gmail.com>
2022-12-17 16:00:00 +0100

    Completes 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n <= 0) return 'OOPS';
+    let a = 1, b = 1;
+    for (let i = 3; i <= n; i++) {
+        let tmp = b;
+        b = a + b;
+        a = tmp;
+    }
+    return b;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+function foo(n) {
+    return 3 <= n;
+}
+console.log(foo("3"));
