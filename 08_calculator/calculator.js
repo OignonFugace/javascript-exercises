a782c95a6195971ac693a89356ab69b5ef277cfb
Tanguy <tanguy.freycon@gmail.com>
2022-12-17 14:55:07 +0100

    Completes 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+    return arg.reduce((sum, item) => sum += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arg) {
+    return arg.reduce((sum, item) => sum *= item);
 };
 
-const power = function() {
-	
+const power = function(m, n) {
+    return m ** n;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    if (n === 1 || n === 0) return 1;
+    return n * factorial(n - 1);
 };
 
 // Do not edit below this line
